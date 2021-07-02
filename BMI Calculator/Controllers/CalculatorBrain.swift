2c89eb5055ba8a10d090ba9c55e1f5c79f4ae0cd
Andre <andreyamasaki@brq.com>
2021-07-02 13:02:51 -0300

    added MVC design pattern, dynamic advice and background color

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by André Yamasaki on 02/07/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    func getBMIValue() -> String {
+        let bmi1ToDecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
+            return bmi1ToDecimalPlace
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "eat more", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "you're doing great", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "eat less", color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1))
+        }
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "not found"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+}
